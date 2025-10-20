bbf1132e57f36ff4c58e2695b81a288743d65406
Saif <sshaikh4@ggc.edu>
2025-10-20 13:49:43 -0400

    pc updates

--- my-Website/src/routes/HomePage.jsx
+++ my-Website/src/routes/HomePage.jsx
@@ -3,7 +3,8 @@ function HomePage(){
     return(
         <div>
             <h1 className="portfolio">PORTFOLIO</h1>
-            
+            <img className="head-img" src="./public/images/headImage.png" alt="head"/>
+
         </div>
     )
 }
