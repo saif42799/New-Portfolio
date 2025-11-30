6207b2b060bd6dd093c0637dfcbc6caa9633246c
saifshaikh <sshaikh4@ggc.edu>
2025-11-30 18:40:19 -0500

    added resume link

--- my-Website/src/routes/HomePage.jsx
+++ my-Website/src/routes/HomePage.jsx
@@ -64,10 +64,12 @@ function HomePage() {
             <img className="head-img" src="/images/headImage.png" alt="head" />
 
             <div className="social-link-container" id="follow-mouse">
-                <div className="resume-box">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" /></svg>
-                    <p>RESUME</p>
-                </div>
+                <a href="https://drive.google.com/file/d/18ibcQP95EFYtrMvlFivXoXFJYCSlmahH/view?usp=sharing" target="blank">
+                    <div className="resume-box">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" /></svg>
+                        <p>RESUME</p>
+                    </div>
+                </a>
 
                 <div className="mail-box" onClick={copyContent}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" /></svg>
